eff26b3f6febe7361783bac00d5bba19fb6f109c
Yuka Ishida <stickey.yy@hotmail.co.jp>
2021-08-01 16:33:52 +0900

    API Token導入対応漏れ

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         return view('home');
