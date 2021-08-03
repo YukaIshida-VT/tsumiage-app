85d32956b23d79c2dd178f8127a8936c2026e4d7
Yuka Ishida <stickey.yy@hotmail.co.jp>
2021-08-03 22:43:58 +0900

    TsumiageItemsController作成

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthUserController;
+use App\Http\Controllers\TsumiageItemsController;
 
 Route::middleware('auth:api')->group(function(){
     Route::get('auth-user', [AuthUserController::class, 'show']);
@@ -10,4 +11,6 @@ Route::middleware('auth:api')->group(function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    Route::post('/items', [TsumiageItemsController::class, 'store']);
 });
