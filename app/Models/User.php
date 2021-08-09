cc2312e5af97571b23f40134b77064d6337a90aa
Yuka Ishida <stickey.yy@hotmail.co.jp>
2021-08-10 00:28:38 +0900

    Roleモデル、factory, seeder作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Tsumiage::class);
     }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
