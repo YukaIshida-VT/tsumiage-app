b38ab6e91b8416f92b1b527ed4e36dbe2f21ff6c
Yuka Ishida <stickey.yy@hotmail.co.jp>
2021-08-10 19:30:27 +0900

    グラフの色は一色に

--- public/js/app.js
+++ public/js/app.js
@@ -1997,8 +1997,7 @@ __webpack_require__.r(__webpack_exports__);
       datasets: [{
         label: 'Bar Dataset',
         data: [10, 20, 30, 40, 50, 30],
-        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+        backgroundColor: 'lightblue',
         borderWidth: 1
       } //   {
       //     label: 'Line Dataset',
