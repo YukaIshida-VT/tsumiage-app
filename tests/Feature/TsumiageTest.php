34a591b6e01bd15c947d86ba85d20a51d4670945
Yuka Ishida <stickey.yy@hotmail.co.jp>
2021-08-15 02:22:56 +0900

    an_authenticated_user_can_add_a_tsumiageテスト追加

--- tests/Feature/TsumiageTest.php
+++ tests/Feature/TsumiageTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Tsumiage;
+use Symfony\Component\HttpFoundation\Response;
 
 class TsumiageTest extends TestCase
 {
@@ -32,8 +33,47 @@ class TsumiageTest extends TestCase
     /** @test **/ 
     public function an_authenticated_user_should_redirected_to_login()
     {
-        $response = $this->post('/api/tsumiage', ['api_token' => '', 'item1' => 'test_item']);
+        $response = $this->post('/api/tsumiage',
+             ['api_token' => '', 'add_item1' => 'test_item', 'date' => '2021-08-15']);
         $response->assertRedirect('/');
         $this->assertCount(0, Tsumiage::all());
+    }
+
+    /** @test **/ 
+    public function an_authenticated_user_can_add_a_tsumiage()
+    {
+        $user = User::factory()->create();
+        $response = $this->post('/api/tsumiage', 
+            ['api_token' => $user->api_token, 'add_item1' => 'item', 'date' => '2021-08-15']);
+
+        $tsumiage = Tsumiage::first();
+
+        $this->assertEquals($tsumiage->item, 'item');
+        $this->assertEquals($tsumiage->user_id, $user->id);
+ 
+        $response->assertStatus(Response::HTTP_CREATED);
+        print_r($response->getdata());
+
+        $response
+            ->assertJson([
+                'data' => [
+                    0 => [
+                        'data' => [
+                            'type' => 'tsumiage',
+                            'tsumiage_id' => $tsumiage->id,
+                            'attributes'=> [
+                                'item' => $tsumiage->item,
+                            ]
+                        ],
+                        'links' => [
+                            'self' => '/tsumiage/' . $tsumiage->id . '/edit',
+                        ]
+                    ],
+                ],
+                'count' => 1,
+                'links' => [
+                    'self' => url('/tsumiage'),
+                ]
+            ]);
     }
 }
